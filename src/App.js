724488f42584e9a6fc586524794c3a726dd5f718
adeniran <adeniranquwam001@gmail.com>
2024-10-24 20:50:27 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import PaystackPayment from './PayStackPayment';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-400 text-3xl'> we just wan start to dey build  </h1>
+      <PaystackPayment />
     </div>
   );
 }
